28416dc206ca2b0f36ddd559c4f0b825e677fa75
awburgard <awburgard@gmail.com>
2019-11-21 08:42:19 -0600

    Initial commit

---
+++ src/components/app/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <code>I Will Teach You To Be Rich</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
